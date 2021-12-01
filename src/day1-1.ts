1f75e35c2849842f052a81cf8f6307b5ec23998f
Clayton Salem <csalem@unleashed-technologies.com>
2021-12-01 10:38:46 -0500

    Day 1 initial

---
+++ src/day1-1.ts
@@ -0,0 +1,25 @@
+import * as fs from "fs";
+import * as path from "path";
+import * as readline from "readline";
+
+const inputFile = path.join(__dirname, "../data/day1.txt");
+
+const rl = readline.createInterface({
+    input: fs.createReadStream(inputFile),
+
+})
+
+let lastDepth = 0;
+let increaseCounter = 0;
+rl.on("line", function(currentDepth) {
+    const depth = Number(currentDepth)
+    if (lastDepth) {
+        // console.log(depth < lastDepth ? "increase": "decrease")
+        if (depth > lastDepth) { increaseCounter++}
+        lastDepth = depth;
+    }
+    else {
+        lastDepth = depth;
+    }
+    console.log("counter", increaseCounter)
+})
